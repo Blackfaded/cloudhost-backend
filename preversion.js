ac6c76abf6bebd33b88601f2bf4d392d6f99b84f
Heinen, Rene <rene.heinen@hsrw.org>
2019-05-07 19:13:10 +0200

    test versioning

--- preversion.js
+++ preversion.js
@@ -1,12 +1,40 @@
 const replace = require('replace-in-file');
 
-const options = {
+const optionsDev = {
 	files: 'docker-compose.dev.yml',
 	from: /cloudhost\/backend_development:(\d+.\d+.\d+)/g,
 	to: `cloudhost/backend_development:${process.env.NPM_PACKAGE_VERSION}`
 };
 
-replace(options)
+const optionsStaging = {
+	files: 'docker-compose.staging.yml',
+	from: /cloudhost\/backend_staging:(\d+.\d+.\d+)/g,
+	to: `cloudhost/backend_staging:${process.env.NPM_PACKAGE_VERSION}`
+};
+
+const optionsProd = {
+	files: 'docker-compose.prod.yml',
+	from: /cloudhost\/backend_production:(\d+.\d+.\d+)/g,
+	to: `cloudhost/backend_production:${process.env.NPM_PACKAGE_VERSION}`
+};
+
+replace(optionsDev)
+	.then((results) => {
+		console.log('Replacement results:', results);
+	})
+	.catch((error) => {
+		console.error('Error occurred:', error);
+	});
+
+replace(optionsStaging)
+	.then((results) => {
+		console.log('Replacement results:', results);
+	})
+	.catch((error) => {
+		console.error('Error occurred:', error);
+	});
+
+replace(optionsProd)
 	.then((results) => {
 		console.log('Replacement results:', results);
 	})
