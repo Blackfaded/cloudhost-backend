a0ced136930ea20d76b0657e2f08c3595dc000da
Heinen, Rene <rene.heinen@hsrw.org>
2019-05-07 18:01:16 +0200

    update domments

--- controllers/docker/container.js
+++ controllers/docker/container.js
@@ -184,7 +184,7 @@ class ContainerController {
 						'traefik.frontend.rule': `Host:${host};PathPrefix:/mongo/${user.email.split('@')[0]}`,
 						/* Service wird nur erreicht, wenn dieser Endpunkt einen HTTP Statuscode mit 2xx zurück gibt.
 						 * Ansonsten wird Traefik den Request abbrechen und eine 401 an den Client zurücksenden.
-						 */
+						 * cloudhost-backend_backend_1 must match the docer container name of the backend api */
 						'traefik.frontend.auth.forward.address':
 							'http://cloudhost-backend_backend_1/api/auth/mongoexpress',
 						'traefik.port': '8081'
